24c1ede5b37276fb231f35a963fa705f3c80c522
Dev <dev@example.com>
2026-04-19 02:52:54 +0800

    chore: 只下载不修改配置

--- scripts/download-db.js
+++ scripts/download-db.js
@@ -1,19 +1,12 @@
 /**
  * 自动下载游戏王数据库
  */
-const { execSync, readFileSync, writeFileSync, existsSync } = require('fs');
+const { execSync, existsSync } = require('fs');
 const { join } = require('path');
 
-const pkgDir = join(__dirname, '..');
-const dbPath = join(pkgDir, 'cards.cdb');
-const yamlPath = join(pkgDir, 'main.yaml');
+const dbPath = join(__dirname, '..', 'cards.cdb');
 const url = 'https://raw.githubusercontent.com/mycard/ygopro-database/master/locales/zh-CN/cards.cdb';
 
 if (!existsSync(dbPath)) {
   execSync(`curl -sL "${url}" -o "${dbPath}"`, { stdio: 'pipe' });
 }
-
-// 修改 path
-let yaml = readFileSync(yamlPath, 'utf8');
-yaml = yaml.replace('path: ./database/locales/zh-CN/cards.cdb', 'path: ./cards.cdb');
-writeFileSync(yamlPath, yaml);
